77fc270ed6a2422d51f9dbd6f4bbdb5323e16f8c
Korben D <imalegalalias@outlook.com>
2019-08-23 18:42:48 +0200

    autokeying_2

---
+++ .config/autokey/data/Scripts/Alt F4.py
@@ -0,0 +1,3 @@
+## hands-on is twisting my thumb in slowmotion
+
+keyboard.send_keys("<alt>+<f4>")
---
+++ .config/autokey/data/Scripts/list Firefox passwords.py
@@ -0,0 +1,15 @@
+## Function to list passwords on Firefox
+## edited from Chris Dardis' https://github.com/autokey/autokey/wiki/Scripting#advanced-scripts
+## 
+
+## menu - edit
+keyboard.send_keys("<ctrl>+t")
+## time.sleep(0.5)
+keyboard.send_keys("about:preferences#privacy")
+time.sleep(0.5)
+keyboard.send_keys("<enter>")
+time.sleep(0.5)
+keyboard.send_keys("<shift>+<alt>+l")
+time.sleep(0.5)
+keyboard.send_keys("<shift>+<alt>+l")
+keyboard.send_keys("<enter>")
