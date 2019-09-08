c43861d1dded697a07d382a907bb4308f7df1519
Korben D <imalegalalias@outlook.com>
2019-09-08 20:12:26 +0200

    autokey thunderbird att. macro

---
+++ .config/autokey/data/Scripts/download att. from tb mail.py
@@ -0,0 +1,14 @@
+
+## menu - message
+keyboard.send_keys("<alt>+m")
+time.sleep(0.3)
+## menu - attachment
+keyboard.send_keys("h")
+time.sleep(0.3)
+## 1 for first (of any)
+keyboard.send_keys("1")
+time.sleep(0.3)
+## a for save as
+keyboard.send_keys("A")
+time.sleep(1.5)
+keyboard.send_keys("<enter>")
